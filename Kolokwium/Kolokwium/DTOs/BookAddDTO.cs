10d46e3739533dc5d9596ccbace2e67955f9f068
s24483 <s24483@pjwstk.edu.pl>
2024-05-07 11:51:25 +0200

    korekta dto

--- Kolokwium/Kolokwium/DTOs/BookAddDTO.cs
+++ Kolokwium/Kolokwium/DTOs/BookAddDTO.cs
@@ -4,10 +4,10 @@ public class BookAddDTO
 {
    
     public string title { get; set; }
-    public List<GenreDTO> genres { get; set; }
+    public List<string> genres { get; set; }
 
     public BookAddDTO()
     {
-        genres = new List<GenreDTO>();
+        genres = new List<string>();
     }
 }
